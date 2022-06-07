53aa73730032cd2044f1baef7990026392241018
MayraMendoza <mayra601m@gmail.com>
2022-06-06 22:03:29 -0400

    Initial commit

---
+++ src/com/careerdevs/JavaAlgorithmPracticeSet1.java
@@ -0,0 +1,16 @@
+package com.careerdevs;
+
+public class JavaAlgorithmPracticeSet1 {
+//    #1
+//    Return the First Element in an Array (getfirstelement.js)
+//    https://edabit.com/challenge/QaApgtePE6QrCZ64o
+    public static int getFirstValue(int[] arr) {
+
+        return arr[0];
+
+    }
+
+   
+
+
+}
